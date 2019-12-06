ba257e4bf651f26b35b25a793cefd45298417bf4
Eloise McIntyre <eloise.mcintyre2@outlook.com>
2019-12-06 16:20:53 +1300

    created dashboard component

---
+++ client/components/Dashboard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+  state = { 
+
+   }
+  render() { 
+    return ( 
+      <>
+        <h1 style = {{marginTop: 150}}>
+          I'm a dashboard!
+        </h1>
+      </>
+     );
+  }
+}
+ 
+export default Dashboard
