f936ed25cdfbfda64ccb97d4208205e1d7a0fd04
Eloise McIntyre <eloise.mcintyre2@outlook.com>
2019-12-06 17:25:57 +1300

    still going

--- client/components/Power.jsx
+++ client/components/Power.jsx
@@ -11,20 +11,20 @@ class Power extends React.Component {
     <Card color='orange'>
       <Image src='/power.png' wrapped ui={false} rounded />
       <Card.Content>
-        <Card.Header>Matthew</Card.Header>
-        <Card.Meta>
+        <Card.Header>DUE IN 15 DAYS</Card.Header>
+        {/* <Card.Meta>
           <span className='date'>Joined in 2015</span>
         </Card.Meta>
         <Card.Description>
           Matthew is a musician living in Nashville.
         </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
+      </Card.Content> */}
+      {/* {/* <Card.Content extra>
         <a>
           <Icon name='user' />
           22 Friends
-        </a>
-      </Card.Content>
+        </a> */}
+      </Card.Content> 
     </Card>
       </>
      )
