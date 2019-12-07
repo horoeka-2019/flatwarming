ceea7abe9b7a911856338834985ff4b54233c1a4
Eloise McIntyre <eloise.mcintyre2@outlook.com>
2019-12-07 14:41:33 +1300

    login component totally restyled

--- client/components/LogIn.jsx
+++ client/components/LogIn.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import { signIn, isAuthenticated } from 'authenticare/client'
-import { Button, Form, Header, Container } from 'semantic-ui-react'
+import { Button, Form, Header, Grid, Segment, Message, Image } from 'semantic-ui-react'
 import { connect } from 'react-redux' 
 import { hideLogin, showLogin, hideReg, showReg, hideLogout, showLogout } from '../actions/nav-buttons'
+import { format } from 'path'
 
 
 function LogIn (props) {
@@ -34,46 +35,34 @@ function LogIn (props) {
         }
       })
   }
+
   return (
     <React.Fragment>
-      <Grid textAlign='center' style={{ height: '100vh', marginTop: 125 }} verticalAlign='middle'>
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as='h2' color='orange' textAlign='center'>
+      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 600 }}>
+          <Header as='h1' color='orange' textAlign='center'>
             <Image src='/favicon.png' /> Log-in to your account
           </Header>
-          <Form size='large'>
+          <Form size='huge'>
             <Segment stacked>
-            <Form.Field>
-            <label>Email</label>
-            <input name='email' type='email'
-              value={form.email}
-              onChange={handleChange}
-            />
-            </Form.Field>
+              
+              <Form.Input 
+                name='email'
+                type='email'
+                value={form.email}
+                onChange={handleChange}
+                fluid 
+                icon='user' 
+                iconPosition='left' 
+                placeholder='E-mail address' 
+              />
 
-            <Form.Field>
-              <label>Password</label>
-              <input name='password' type='password'
+              <Form.Input
+                style={{fontFamily: 'Icons'}}
+                name='password'
+                type='password'
                 value={form.password}
                 onChange={handleChange}
-              />
-            </Form.Field>
-
-            <Form.Field type='button' onClick={handleClick}
-              control={Button}
-              disabled={
-                !form.password ||
-                !form.email ||
-                !form.email.includes('@')
-              }
-            >Login
-            </Form.Field>
-
-          </Form>
-
-
-              {/* <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-              <Form.Input
                 fluid
                 icon='lock'
                 iconPosition='left'
@@ -81,24 +70,28 @@ function LogIn (props) {
                 type='password'
               />
 
-              <Button color='teal' fluid size='large'>
+              <Button 
+                color='orange' 
+                fluid size='large'
+                onClick={handleClick}
+                disabled={
+                !form.password ||
+                !form.email ||
+                !form.email.includes('@')
+                }
+              >
                 Login
               </Button>
             </Segment>
-          </Form> */}
+          </Form>
           <Message>
-            New to us? <a href='#'>Sign Up</a>
+            New to us? <a href='/register'>Register</a>
           </Message>
         </Grid.Column>
       </Grid>
 
-      {/* <Container text style={{ border: '1px', borderStyle: 'solid', padding: '30px', marginTop: 125, maxWidth: '40vw' }}>
 
-        <Header as='h2' textAlign='center'>Log In</Header>
-
-        <Form size = 'large'> */}
-
-          <Form.Field>
+          {/* <Form.Field>
             <label>Email</label>
             <input name='email' type='email'
               value={form.email}
@@ -125,7 +118,7 @@ function LogIn (props) {
           </Form.Field>
 
         </Form>
-      </Container>
+      </Container> */}
     </React.Fragment>
   )
 }
