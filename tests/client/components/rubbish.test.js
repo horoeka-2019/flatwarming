66b6c9ca90f732e20d431859a8c89b52b3fdf83b
Eloise McIntyre <eloise.mcintyre2@outlook.com>
2019-12-07 16:01:45 +1300

    finished a simple Water component test

--- tests/client/components/rubbish.test.js
+++ tests/client/components/rubbish.test.js
@@ -1,14 +1,7 @@
 import React from 'react'
 import { mount } from 'enzyme'
-import toJson from 'enzyme-to-json'
-// import { Provider } from 'react-redux'
-
-// import configureStore from 'redux-mock-store'
 
 import Rubbish from '../../../client/components/Rubbish'
-
-// const middleware = []
-// const mockStore = configureStore(middleware)
 
 describe('<Rubbish /> component tests', () => {
   it('contains "RUBBISH DAY"', () => {
