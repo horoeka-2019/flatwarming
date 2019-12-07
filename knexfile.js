8a663cc960087e8939ec7f32ceec1ac07e0e7667
Sunny Niu <sunnyyingli@gmail.com>
2019-12-07 21:53:49 +1300

    try

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
     connection: {
       filename: './dev.sqlite3'
     },
-    useNullAsDefault:true
+    useNullAsDefault: true
   },
 
   test: {
@@ -26,36 +26,17 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: '_migrations',
+      directory: path.join(__dirname, 'migrations')
     }
   }
 
-};
+}
