f35a8dfe26bc4960a81d3d293483557002618a0f
Ashleigh Dominique Ah Ta <ashleigh.ahta@gmail.com>
2019-12-10 09:58:37 +1300

    correct catch on line 40

--- client/components/LogIn.jsx
+++ client/components/LogIn.jsx
@@ -1,16 +1,14 @@
 import React, { useState } from 'react'
 import { signIn, isAuthenticated } from 'authenticare/client'
 import { Button, Form, Header, Grid, Segment, Message, Image } from 'semantic-ui-react'
-import { connect } from 'react-redux' 
+import { connect } from 'react-redux'
 import { hideLogin, showLogin, hideReg, showReg, hideLogout, showLogout } from '../actions/nav-buttons'
 import Footer from './Footer'
 import { getUserByName } from '../api/registerFlatDetails'
 
 import { setError } from '../actions/error'
 
-
 function LogIn (props) {
-  
   const [form, setForm] = useState({
     username: '',
     password: ''
@@ -39,63 +37,60 @@ function LogIn (props) {
           props.dispatch(showLogout())
         }
       })
-    .catch(setError)
-      if (!isAuthenticated()) {
-        props.dispatch(setError)
-      }   
+      .catch(err => props.setError('Oops! Are you Trying to sign-up? Press Register!  ', err))
   }
 
   return (
     <>
-      <Grid textAlign='center' style={{ height: '80vh' }} verticalAlign='middle'>
-        <Grid.Column style={{ maxWidth: 600 }}>
-          <Header as='h1' color='orange' textAlign='center'>
-            <Image src='/favicon.png' /> Log-in to your account
-          </Header>
-          <Form size='huge'>
-            <Segment stacked>
-              
-              <Form.Input
-                name='email'
-                type='email'
-                value={form.email}
-                onChange={handleChange}
-                fluid
-                icon='user'
-                iconPosition='left'
-                placeholder='E-mail address'
-              />
+    <Grid textAlign='center' style={{ height: '80vh' }} verticalAlign='middle'>
+      <Grid.Column style={{ maxWidth: 600 }}>
+        <Header as='h1' color='orange' textAlign='center'>
+          <Image src='/favicon.png' /> Log-in to your account
+        </Header>
+        <Form size='huge'>
+          <Segment stacked>
 
-              <Form.Input
-                name='password'
-                type='password'
-                value={form.password}
-                onChange={handleChange}
-                fluid
-                icon='lock'
-                iconPosition='left'
-                placeholder='Password'
-              />
+            <Form.Input
+              name='email'
+              type='email'
+              value={form.email}
+              onChange={handleChange}
+              fluid
+              icon='user'
+              iconPosition='left'
+              placeholder='E-mail address'
+            />
 
-              <Button
-                color='orange'
-                fluid size='large'
-                onClick={handleClick}
-                disabled={
-                  !form.password ||
+            <Form.Input
+              name='password'
+              type='password'
+              value={form.password}
+              onChange={handleChange}
+              fluid
+              icon='lock'
+              iconPosition='left'
+              placeholder='Password'
+            />
+
+            <Button
+              color='orange'
+              fluid size='large'
+              onClick={handleClick}
+              disabled={
+                !form.password ||
                   !form.email ||
                   !form.email.includes('@')
-                }
-              >
+              }
+            >
                 Login
-              </Button>
-            </Segment>
-          </Form>
-          <Message>
+            </Button>
+          </Segment>
+        </Form>
+        <Message>
             New to us? <a href='/register'>Register</a>
-          </Message>
-        </Grid.Column>
-      </Grid>
+        </Message>
+      </Grid.Column>
+    </Grid>
     <Footer />
     </>
   )
@@ -110,4 +105,8 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(LogIn)
+const mapDispatchToProps = {
+  setError
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LogIn)
