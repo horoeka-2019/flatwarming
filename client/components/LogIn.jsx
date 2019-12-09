9d1bef454112968cd78b4b6a1e9ff058889c25b9
Ashleigh Dominique Ah Ta <ashleigh.ahta@gmail.com>
2019-12-10 10:46:38 +1300

    fix typo in error message

--- client/components/LogIn.jsx
+++ client/components/LogIn.jsx
@@ -37,7 +37,7 @@ function LogIn (props) {
           props.dispatch(showLogout())
         }
       })
-      .catch(err => props.setError('Oops! Are you Trying to sign-up? Press Register! ', err))
+      .catch(err => props.setError('Oops! Are you trying to sign-up? Press Register! ', err))
   }
 
   return (
